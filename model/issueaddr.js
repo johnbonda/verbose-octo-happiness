bda0c40ba79103b5979184352e3a84dc3b25ddbb
johnbonda <john@belfricsbt.com>
2018-12-11 17:12:37 +0530

    lavda commit

---
+++ model/issueaddr.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'issueaddrs',
+    fields: [
+      {
+        name: 'address',
+        type: 'String',
+        length: 255
+      },
+      {
+        name: 'dappid',
+        type: 'String',
+        length: 255
+      }
+    ]
+  }
---
+++ model/mapping.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: 'mappings',
+    fields: [
+      {
+        name: 'email',
+        type: 'String',
+        length: 256
+      },
+      {
+        name: 'dappid',
+        type: 'String',
+        length: 255
+      },
+      {
+        name: 'role',
+        type: 'String',
+        length: 255
+      }
+    ]
+  }
